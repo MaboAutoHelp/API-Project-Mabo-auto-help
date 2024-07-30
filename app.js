62f72d7ca6898ab0f6a36705525e09fdb2a97772
mohamed-aziz <medazizzammal@gmail.com>
2024-07-30 16:43:11 +0100

    Connected to database

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const db = require('./db')
+const userRouter = require('./routes/userRouter')
 
 const app = express()
+
+app.use('/test',userRouter)
 
 app.listen(8000,()=>{
     console.log('Server is running on port 8000')
