5b0b13ed2ceeb738ae25f9802810f3ab3ef6e485
mohamed-aziz <medazizzammal@gmail.com>
2024-07-31 10:01:38 +0100

    chore: Update database connection string and add dotenv dependency

--- db.js
+++ db.js
@@ -1,9 +1,13 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://maboautohelp:ZN7KVqHYmlnpOtXe@firstapi.wwwee8b.mongodb.net/?retryWrites=true&w=majority&appName=firstapi')
-.then(()=>{
-    console.log('Connected to database')
+// Use template literals to concatenate the connection string
+const password = process.env.PWD;
+const uri = `mongodb+srv://maboautohelp:${password}@firstapi.wwwee8b.mongodb.net/?retryWrites=true&w=majority&appName=firstapi`;
+
+mongoose.connect(uri)
+.then(() => {
+    console.log('Connected to database');
 })
-.catch(()=>{
-    console.log('Unable to Connected to database ')
-})
+.catch(() => {
+    console.log('Unable to connect to database');
+});
