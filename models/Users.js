840bbe899655cec083f0305f269a910d315b6556
mohamed-aziz <medazizzammal@gmail.com>
2024-07-31 20:40:11 +0100

    chore: Update database connection string, add dotenv dependency, and refactor user routes
    ```

---
+++ models/Users.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose")
+
+const UsersSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required:true
+    },
+    email:{
+        type: String,
+    },
+    pwd:{
+        type: String,
+        required:true
+    }
+})
+
+
+
+const UsersModel= mongoose.model("users",UsersSchema)
+module.exports = UsersModel
