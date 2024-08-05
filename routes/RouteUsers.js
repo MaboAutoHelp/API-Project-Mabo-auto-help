5dce7e958a7782c6e5cfbf3624e1574295d99e2c
mohamed-aziz <medazizzammal@gmail.com>
2024-08-05 11:35:27 +0100

    chore: Refactor user routes and add telephone number field to user registration

--- routes/RouteUsers.js
+++ routes/RouteUsers.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { register ,login } = require('../controllers/UsersControoller');
+const { register ,login,updateUser } = require('../controllers/UsersControoller');
 
 router.post('/register', register);
 router.post('/login',login)
+router.put('/updateUser/:UserId',updateUser)
 
 
 module.exports = router;
