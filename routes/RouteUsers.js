8b91885df3f9f578359d5984fc7b99cff6f69ef8
mohamed-aziz <medazizzammal@gmail.com>
2024-08-05 23:12:03 +0100

    chore: Add getUser route and controller

--- routes/RouteUsers.js
+++ routes/RouteUsers.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { register ,login,updateUser } = require('../controllers/UsersControoller');
+const { register ,login,updateUser,getUser } = require('../controllers/UsersControoller');
 
 router.post('/register', register);
 router.post('/login',login)
 router.put('/updateUser/:UserId',updateUser)
+router.get('/getUser/:UserId',getUser)
 
 
 module.exports = router;
