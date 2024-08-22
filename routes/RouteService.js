0864e777fc1f89588409daf00a137657237f5d41
mohamed-aziz <medazizzammal@gmail.com>
2024-08-22 23:03:45 +0100

    chore: Refactor ServiceController and Service model, add new routes and controllers

--- routes/RouteService.js
+++ routes/RouteService.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { registerService,getAllService,getAllServices ,updateService,getAllServicesAccepted,getAllServicesRejected,getServiceID} = require('../controllers/ServiceController');
+const { registerService,
+    getAllService,getAllServices ,updateService,getAllServicesAccepted,
+    getAllServicesRejected,getServiceID,deletedService,getAllServicesAttente,
+    updateServiceIta,getServiceMicaniciens} = require('../controllers/ServiceController');
 
 router.post('/registerService', registerService);
 router.get('/getAllService/:id', getAllService);
@@ -9,6 +12,10 @@ router.put('/updateService/:ID', updateService);
 router.get('/getAllServicesAccepted', getAllServicesAccepted);
 router.get('/getAllServicesRejected', getAllServicesRejected);
 router.get('/getServiceID/:id', getServiceID);
+router.delete('/deletedService', deletedService);
+router.get('/getAllServicesAttente', getAllServicesAttente);
+router.put('/updateServiceIta/:ID', updateServiceIta);
+router.get('/getServiceMicaniciens/:id', getServiceMicaniciens);
 
 
 
