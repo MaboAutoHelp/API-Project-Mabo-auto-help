aabd85cb0b027a4d6ad180ef824043352c8bba4f
mohamed-aziz <medazizzammal@gmail.com>
2024-10-25 23:19:58 +0100

    feat: Add getListID and updateList functionality to ListServiceController and RouteListService

--- routes/RouteListService.js
+++ routes/RouteListService.js
@@ -1,9 +1,11 @@
 const express= require('express')
 const router = express.Router();
 
-const {register,getVservice} =require('../controllers/ListServiceController')
+const {register,getVservice,getListID,updateList} =require('../controllers/ListServiceController')
 
 router.post('/register',register)
 router.get('/getVservice/:V',getVservice)
+router.get('/getListID/:id',getListID)
+router.put('/updateList/:id',updateList)
 
 module.exports= router
